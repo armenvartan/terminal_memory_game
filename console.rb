19e8a0915c1362dd59ed95770426503b4e62865f
Armen Vartan <vartan.armen@gmail.com>
2014-06-13 20:19:07 -0400

    moved functions to console module

--- console.rb
+++ console.rb
@@ -5,6 +5,21 @@ module Console
     puts "\e[H\e[2J"
   end
 
+  def letter_string
+    self.join
+  end
+
+  def random_letter
+    self.sample
+  end
+
+  def add_letter
+    self << random_letter
+  end
+
+  def compare(answer)
+    answer.upcase == letter_string
+  end
 
 
 end
